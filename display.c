18ddf6d44c975c4d948025778f0a5e127d1c366b
Ricky Liou <rliou92@gmail.com>
2016-08-15 20:10:40 -0700

    Saving works, also delete
    Starting to work on loading

--- display.c
+++ display.c
@@ -7,37 +7,40 @@
 #include <libconfig.h>
 
 // TODO Make everything into functions
+//
+Display* myDisp;
+Window myWin;
+XRRMonitorInfo* myMon;
+XRRScreenResources* myScreen;
+XRROutputInfo* myOutput;
+XRRPropertyInfo* myProp;
+XEvent event;
+Atom edid_atom;
+char* display_name = 0;
+int i,j,k,l,z;
+int save = 0;
+int load = 0;
+int delete = 0;
+int numMon,nprop;
+int quiet = 0;
+int cfg_idx;
+Bool get_active = 0;
+char edid_name[] = "EDID";
+unsigned char* edid,*edid_string;
+Bool only_if_exists = 1;
+unsigned char* prop;
+int actual_format;
+unsigned long nitems,bytes_after;
+Atom actual_type;
+XRRCrtcInfo* myCrtc;
+char config_file[] = "umon.conf";
+char* profile_name;
+config_setting_t *edid_setting,*resolution_setting,*pos_x_setting,*pos_y_setting,*pos_group,*root,*group,*list;
+config_t config;
+char* edid_val,resolution_val;
+int pos_val[2];
 
 int main(int argc, char **argv) {
-	Display* myDisp;
-	Window myWin;
-	XRRMonitorInfo* myMon;
-	XRRScreenResources* myScreen;
-	XRROutputInfo* myOutput;
-	XRRPropertyInfo* myProp;
-	XEvent event;
-	Atom edid_atom;
-	char* display_name = 0;
-	int i,j,k,l,z;
-	int save = 0;
-	int load = 0;
-	int numMon,nprop;
-	int quiet = 0;
-	int i2cbus = -1;
-	int cfg_idx;
-	Bool get_active = 0;
-	char edid_name[] = "EDID";
-	unsigned char* edid,*edid_string;
-	Bool only_if_exists = 1;
-	unsigned char* prop;
-	int actual_format;
-	unsigned long nitems,bytes_after;
-	Atom actual_type;
-	XRRCrtcInfo* myCrtc;
-	char config_file[] = "umon.conf";
-	char* profile_name;
-	config_setting_t *edid_setting,*resolution_setting,*pos_x_setting,*pos_y_setting,*pos_group,*root,*group,*list;
-	config_t config;
 
 	if (argc == 3) {
 		if (!strcmp("--save", argv[1])) {
@@ -45,6 +48,9 @@ int main(int argc, char **argv) {
 		}
 		else if (!strcmp("--load", argv[1])) {
 			load = 1;
+		}
+		else if (!strcmp("--delete", argv[1])){
+			delete = 1;
 		}
 
 		profile_name = argv[2];
@@ -55,9 +61,8 @@ int main(int argc, char **argv) {
 			printf("Detected existing configuration file\n");
 			// Existing config file to load setting values
 			if (load) {
-				/*
-				group = config_lookup(&config,profile_name);
-				if (group != NULL) {
+				list = config_lookup(&config,profile_name);
+				if (list != NULL) {
 					edid_setting = config_setting_get_string(group,"EDID",CONFIG_TYPE_STRING);
 					resolution_setting = config_setting_add(group,"resolution",CONFIG_TYPE_STRING);
 					pos_group = config_setting_add(group,"pos",CONFIG_TYPE_GROUP);
@@ -69,46 +74,27 @@ int main(int argc, char **argv) {
 					// No profile with the specified name
 					exit(2);
 				}
-				*/
 			}
 			// Overwrite existing profile
-			if (save) {
+			if (save || delete) {
 
 				list = config_lookup(&config,profile_name);
 				if (list != NULL) {
 					printf("Existing profile found, overwriting\n");
 					cfg_idx = config_setting_index(list);
 					root = config_setting_parent(list);
-					printf("Configuration index: %d\n",cfg_idx);
+					// printf("Configuration index: %d\n",cfg_idx);
 					config_setting_remove_elem(root,cfg_idx);
 				}
-				// Make new profile 
-				// TODO: Make into function
-				root = config_root_setting(&config);
-				list = config_setting_add(root,profile_name,CONFIG_TYPE_LIST);
-				group = config_setting_add(list,NULL,CONFIG_TYPE_GROUP);
-				edid_setting = config_setting_add(group,"EDID",CONFIG_TYPE_STRING);
-				resolution_setting = config_setting_add(group,"resolution",CONFIG_TYPE_STRING);
-				pos_group = config_setting_add(group,"pos",CONFIG_TYPE_GROUP);
-				pos_x_setting = config_setting_add(pos_group,"x",CONFIG_TYPE_INT);
-				pos_y_setting = config_setting_add(pos_group,"y",CONFIG_TYPE_INT);
 			}
 		}
 		else {
-			if (load) exit(1); // No file to load
-			if (save) {
-				root = config_root_setting(&config);
-				list = config_setting_add(root,profile_name,CONFIG_TYPE_LIST);
-				group = config_setting_add(list,NULL,CONFIG_TYPE_GROUP);
-				edid_setting = config_setting_add(group,"EDID",CONFIG_TYPE_STRING);
-				resolution_setting = config_setting_add(group,"resolution",CONFIG_TYPE_STRING);
-				pos_group = config_setting_add(group,"pos",CONFIG_TYPE_GROUP);
-				pos_x_setting = config_setting_add(pos_group,"x",CONFIG_TYPE_INT);
-				pos_y_setting = config_setting_add(pos_group,"y",CONFIG_TYPE_INT);
-			}
+			if (load || delete) exit(1); // No file to load or delete
 		}
 		if (save) {
-			// TODO Fix the logic to make multiple groups for multiple outputs
+			root = config_root_setting(&config);
+			list = config_setting_add(root,profile_name,CONFIG_TYPE_LIST);
+
 			// Fetch current configuration info
 			myDisp = XOpenDisplay(display_name);
 			myWin = DefaultRootWindow(myDisp);
@@ -129,6 +115,15 @@ int main(int argc, char **argv) {
 				myOutput = XRRGetOutputInfo(myDisp,myScreen,myScreen->outputs[i]);
 				// printf("Name: %s Connection %d\n",myOutput->name,myOutput->connection);
 				if (!myOutput->connection) {
+
+					// Setup config
+					group = config_setting_add(list,NULL,CONFIG_TYPE_GROUP);
+					edid_setting = config_setting_add(group,"EDID",CONFIG_TYPE_STRING);
+					resolution_setting = config_setting_add(group,"resolution",CONFIG_TYPE_STRING);
+					pos_group = config_setting_add(group,"pos",CONFIG_TYPE_GROUP);
+					pos_x_setting = config_setting_add(pos_group,"x",CONFIG_TYPE_INT);
+					pos_y_setting = config_setting_add(pos_group,"y",CONFIG_TYPE_INT);
+
 					XRRGetOutputProperty(myDisp,myScreen->outputs[i],edid_atom,0,100,False,False,AnyPropertyType,&actual_type,&actual_format,&nitems,&bytes_after,&edid);
 					// printf("%d",nitems);
 					if (nitems) {
@@ -136,11 +131,16 @@ int main(int argc, char **argv) {
 						// Make edid into string
 						edid_string = (unsigned char *) malloc((nitems+1) * sizeof(char));
 						for (z=0;z<nitems;++z) {
-							edid_string[z] = edid[z];
-							printf("%c",edid[z]);
+							if (edid[z] == '\0') {
+								edid_string[z] = '0';
+							}
+							else {
+								edid_string[z] = edid[z];
+							}
+							//printf("%c",edid_string[z]);
 						}
 						printf("\n");
-						edid_string[nitems] = 0;
+						edid_string[nitems] = '\0';
 						printf("%s",edid_string);
 						// Get current mode
 						for (j=0;j<myOutput->nmode;++j) {
@@ -170,16 +170,17 @@ int main(int argc, char **argv) {
 					}
 				}
 			}
-			config_write_file(&config,config_file);
-			config_destroy(&config);
 			free(myCrtc);
 		}
+		config_write_file(&config,config_file);
+		config_destroy(&config);
 	}
 	
 
 // 	while (1){
 // 		XNextEvent(myDisp, (XEvent *) &event);
 // 		printf ("Event received, type = %d\n", event.type);
+// 		XRRUpdateConfiguration?
 // 	}
 
 }
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
 # rm umon.conf
-make clean && make && ./display --save home
+make clean && make && printf "\n" && ./display --save home
