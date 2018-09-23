9cc20fc69714408e8727118a62e93932222d6d0d
Ricky Liou <rliou92@gmail.com>
2018-09-22 19:00:17 -0700

    Use existing crtcs as candidate crtc.
    If output candidate is 0, then find the next feasible crtc to use.

--- src/load.c
+++ src/load.c
@@ -25,6 +25,12 @@ typedef struct _crtc_ll {
 	struct _crtc_ll *next;
 } crtc_ll;
 
+typedef struct _output_reply_ll {
+	xcb_randr_get_output_info_reply_t *output_info_reply;
+	xcb_randr_output_t * output_p;
+	struct _output_reply_ll *next;
+} output_reply_ll;
+
 /*! Screen information that is saved into configuration file
 */
 typedef struct {
@@ -99,8 +105,8 @@ struct find_available_crtc_param_t {
 };
 
 struct determine_crtc_match_param_t {
-	int *already_assigned_p, *i_p, *j_p;
-	xcb_randr_crtc_t *crtc_p, *available_crtcs, *output_crtcs;
+	int num_crtcs;
+	xcb_randr_crtc_t *crtc_p, *output_crtcs;
 };
 
 static void set_force_load(load_class * self, int force_load);
@@ -108,12 +114,15 @@ static void get_cur_loaded(load_class * self, int *cur_loaded);
 static void get_last_time(load_class * self, xcb_timestamp_t * last_time);
 static void free_crtc_param_ll(set_crtc_param * head);
 static void free_crtc_ll(crtc_ll * crtc_ll_head);
+static void free_output_reply_ll(output_reply_ll * reply_ll_head);
 static void load_profile(load_class * self,
 			 config_setting_t * profile_group, int);
 static void load_config_val(load_class * self,
 			    config_setting_t * profile_group);
-static void match_with_config(void *self_void,
-			      xcb_randr_output_t * output_p);
+static void construct_output_reply_ll(void *self_void,
+				      xcb_randr_output_t *output_p);
+static void match_with_config(load_class *self,
+	output_reply_ll *);
 static void find_mode_id(load_class * self,
 			 struct find_mode_id_param_t *param);
 static void determine_mode_id_match(load_class * self, struct determine_mode_id_match_param_t
@@ -153,6 +162,7 @@ typedef struct {
 	set_crtc_param *crtc_param_head;
 	crtc_ll *assigned_crtc_head;
 	crtc_ll *disable_crtc_head;
+	output_reply_ll *output_reply_head;
 
 	int cur_loaded;
 	int num_out_pp;
@@ -181,6 +191,7 @@ void load_class_constructor(load_class ** self_p, screen_class * screen_o)
 	PVAR->cur_loaded = 0;
 	PVAR->assigned_crtc_head = NULL;
 	PVAR->force_load = 0;
+	PVAR->output_reply_head = NULL;
 
 	*self_p = self;
 
@@ -234,6 +245,19 @@ static void free_crtc_ll(crtc_ll * crtc_ll_head)
 	}
 }
 
+static void free_output_reply_ll(output_reply_ll * reply_ll_head)
+{
+	output_reply_ll *cur_free_reply, *old_free_reply;
+	cur_free_reply = reply_ll_head;
+	while (cur_free_reply) {
+		old_free_reply = cur_free_reply;
+		cur_free_reply = cur_free_reply->next;
+		free(old_free_reply->output_info_reply);
+		free(old_free_reply);
+	}
+}
+
+
 /*! \brief Load the specified profile
  */
 
@@ -244,6 +268,8 @@ static void load_profile(load_class * self,
 	struct modify_crtc_ll_param_t modify_crtc_ll_param;
 	xcb_randr_crtc_t *crtcs_p;
 	char *profile_name;
+	output_reply_ll *cur_output_reply;
+	crtc_ll *new_assigned_crtc;
 
 	PVAR->disable_crtc_head = NULL;
 	load_config_val(self, profile_group);
@@ -252,10 +278,30 @@ static void load_profile(load_class * self,
 	umon_print("Num outputs per profile %s: %d\n", profile_name,
 		   PVAR->num_out_pp);
 
-	// Queue up crtc linked list to load
+	// Construct output reply linked list
 	for_each_output((void *) self,
 			PVAR->screen_o->screen_resources_reply,
-			match_with_config);
+			construct_output_reply_ll);
+
+	// Find the crtcs that are being used
+	cur_output_reply = PVAR->output_reply_head;
+	while (cur_output_reply) {
+		if (cur_output_reply->output_info_reply->crtc != 0) {
+			new_assigned_crtc = (crtc_ll *)
+			    umalloc(sizeof(crtc_ll));
+			new_assigned_crtc->crtc = cur_output_reply->output_info_reply->crtc;
+			new_assigned_crtc->next = PVAR->assigned_crtc_head;
+			PVAR->assigned_crtc_head = new_assigned_crtc;
+		}
+		cur_output_reply = cur_output_reply->next;
+	}
+
+	// Queue up crtc linked list to load
+	cur_output_reply = PVAR->output_reply_head;
+	while (cur_output_reply) {
+		match_with_config(self, cur_output_reply);
+		cur_output_reply = cur_output_reply->next;
+	}
 	// Check against redundant loading
 	//TODO Only assuming 1 output connected to crtc
 	// Examine current configuration state to determine which crtcs should actually be loaded and which should be disabled
@@ -285,25 +331,22 @@ static void load_profile(load_class * self,
 	}
 
 	free_crtc_ll(PVAR->assigned_crtc_head);
+	free_output_reply_ll(PVAR->output_reply_head);
 
 	PVAR->crtc_param_head = NULL;
 	PVAR->disable_crtc_head = NULL;
 	PVAR->assigned_crtc_head = NULL;
+	PVAR->output_reply_head = NULL;
 }
 
-static void match_with_config(void *self_void,
+static void construct_output_reply_ll(void *self_void,
 			      xcb_randr_output_t * output_p)
 {
-
-	int conf_output_idx;
 	xcb_randr_get_output_info_cookie_t output_info_cookie;
 	xcb_randr_get_output_info_reply_t *output_info_reply;
-	struct find_mode_id_param_t mode_id_param;
-	char *output_name;
+	output_reply_ll *new_output_reply;
 
 	load_class *self = (load_class *) self_void;
-
-	char *edid_string;
 
 	output_info_cookie =
 	    xcb_randr_get_output_info(PVAR->screen_o->c, *output_p,
@@ -314,21 +357,39 @@ static void match_with_config(void *self_void,
 					    output_info_cookie,
 					    &PVAR->screen_o->e);
 
-	// Should fetch edid of every output, just the outputs that have connection
 	if (output_info_reply->connection) {
 		free(output_info_reply);
 		return;
 	}
 
-	get_output_name(output_info_reply, &output_name);
-	fetch_edid(output_p, PVAR->screen_o, &edid_string);
+	new_output_reply = (output_reply_ll *) umalloc(sizeof(output_reply_ll));
+	new_output_reply->output_info_reply = output_info_reply;
+	new_output_reply->output_p = output_p;
+	new_output_reply->next = PVAR->output_reply_head;
+	PVAR->output_reply_head = new_output_reply;
+
+
+}
+
+
+static void match_with_config(load_class *self,
+	output_reply_ll *cur_output_reply)
+{
+
+	int conf_output_idx;
+	struct find_mode_id_param_t mode_id_param;
+	char *output_name;
+	char *edid_string;
+
+	get_output_name(cur_output_reply->output_info_reply, &output_name);
+	fetch_edid(cur_output_reply->output_p, PVAR->screen_o, &edid_string);
 	umon_print
 	    ("Trying to find matching setting for %s (%s)\n",
 	     output_name, edid_string);
 
 	mode_id_param.conf_output_idx_p = &conf_output_idx;
-	mode_id_param.output_info_reply = output_info_reply;
-	mode_id_param.cur_output = output_p;
+	mode_id_param.output_info_reply = cur_output_reply->output_info_reply;
+	mode_id_param.cur_output = cur_output_reply->output_p;
 	for (conf_output_idx = 0; conf_output_idx < PVAR->num_out_pp;
 	     ++conf_output_idx) {
 		// umon_print("")
@@ -347,7 +408,6 @@ static void match_with_config(void *self_void,
 		}
 	}
 	free(edid_string);
-	free(output_info_reply);
 	free(output_name);
 }
 
@@ -491,7 +551,7 @@ static void add_crtc_param(load_class * self,
 
 }
 
-/*! \brief Find an available crtc for the current output with a specified offset
+/*! \brief Find an available crtc for the current output
  */
 
 static void find_available_crtc(load_class * self,
@@ -499,84 +559,81 @@ static void find_available_crtc(load_class * self,
 				xcb_randr_crtc_t * crtc_p)
 {
 	struct determine_crtc_match_param_t crtc_match_param;
-	int i, j, already_assigned, num_output_crtcs, num_available_crtcs;
-	xcb_randr_crtc_t *available_crtcs, *output_crtcs;
+	xcb_randr_crtc_t *output_crtcs;
 
-	already_assigned = 1;
-	available_crtcs =
-	    xcb_randr_get_screen_resources_crtcs(PVAR->
-						 screen_o->screen_resources_reply);
+	// already_assigned = 1;
+	// available_crtcs =
+	//     xcb_randr_get_screen_resources_crtcs(PVAR->
+	// 					 screen_o->screen_resources_reply);
 
 	output_crtcs =
 	    xcb_randr_get_output_info_crtcs(param->output_info_reply);
 
-	num_output_crtcs =
-	    xcb_randr_get_output_info_crtcs_length
-	    (param->output_info_reply);
+	umon_print("Candidate crtc for this output: %d\n", param->output_info_reply->crtc);
 
-	num_available_crtcs =
-	    PVAR->screen_o->screen_resources_reply->num_crtcs;
-
-	crtc_match_param.crtc_p = crtc_p;
-	crtc_match_param.i_p = &i;
-	crtc_match_param.j_p = &j;
-	crtc_match_param.already_assigned_p = &already_assigned;
-	crtc_match_param.available_crtcs = available_crtcs;
-	crtc_match_param.output_crtcs = output_crtcs;
-	for (i = 0; i < num_available_crtcs; ++i) {
-		for (j = 0; j < num_output_crtcs; ++j) {
-			determine_crtc_match(self, &crtc_match_param);
-			if (!already_assigned) {
-				return;
-			}
-		}
+	if (param->output_info_reply->crtc != 0) {
+		*(crtc_p) = param->output_info_reply->crtc;
+		return;
 	}
 
-	umon_print("ERROR: No available crtc found\n");
-	exit(NO_AVAILABLE_CRTC);
+	crtc_match_param.crtc_p = crtc_p;
+	crtc_match_param.num_crtcs = param->output_info_reply->num_crtcs;
+	crtc_match_param.output_crtcs = output_crtcs;
+
+	determine_crtc_match(self, &crtc_match_param);
+	umon_print("Candidate crtc will not work, using %d instead\n", *crtc_p);
+
+
+	// num_output_crtcs =
+	//     xcb_randr_get_output_info_crtcs_length
+	//     (param->output_info_reply);
+	//
+	// num_available_crtcs =
+	//     PVAR->screen_o->screen_resources_reply->num_crtcs;
+	//
+	// crtc_match_param.crtc_p = crtc_p;
+	// crtc_match_param.i_p = &i;
+	// crtc_match_param.j_p = &j;
+	// crtc_match_param.already_assigned_p = &already_assigned;
+	// crtc_match_param.available_crtcs = available_crtcs;
+	// crtc_match_param.output_crtcs = output_crtcs;
+	// for (i = 0; i < num_available_crtcs; ++i) {
+	// 	for (j = 0; j < num_output_crtcs; ++j) {
+	// 		determine_crtc_match(self, &crtc_match_param);
+	// 		if (!already_assigned) {
+	// 			return;
+	// 		}
+	// 	}
+	// }
+	//
 
 }
 
 static void determine_crtc_match(load_class * self, struct determine_crtc_match_param_t
 				 *param)
 {
-	int i, j;
-	crtc_ll *new_assigned_crtc, *cur_assigned_crtc;
+	int i, already_assigned;
+	crtc_ll *cur_assigned_crtc;
 
-	i = *(param->i_p);
-	j = *(param->j_p);
-
-	if (param->available_crtcs[i] != param->output_crtcs[j])
-		return;
-
-	*(param->already_assigned_p) = 0;
-	umon_print
-	    ("Found potential crtc %d. Is it already assigned?\n",
-	     param->available_crtcs[i]);
-	cur_assigned_crtc = PVAR->assigned_crtc_head;
-	// printf("Assigned crtc head: %d\n",assigned_crtc_head);
-	while (cur_assigned_crtc) {
-		//umon_print
-		//("cur_assigned_crtc %d\n", cur_assigned_crtc->crtc);
-		if (cur_assigned_crtc->crtc == param->available_crtcs[i]) {
-			umon_print
-			    ("Crtc %d already assigned!\n",
-			     param->available_crtcs[i]);
-			*(param->already_assigned_p) = 1;
-			// TODO can optimize here with break or return?
+	for (i = 0; i < param->num_crtcs; ++i) {
+		already_assigned = 0;
+		cur_assigned_crtc = PVAR->assigned_crtc_head;
+		while (cur_assigned_crtc) {
+			if (cur_assigned_crtc->crtc == param->output_crtcs[i]) {
+				// umon_print
+				//     ("cur_assigned_crtc: %d, param->output_crtcs[i]: %d\n",
+				//      cur_assigned_crtc->crtc, param->output_crtcs[i]);
+				already_assigned = 1;
+			}
+			cur_assigned_crtc = cur_assigned_crtc->next;
 		}
-		cur_assigned_crtc = cur_assigned_crtc->next;
+		if (already_assigned == 0) {
+			*(param->crtc_p) = param->output_crtcs[i];
+			return;
+		}
 	}
-	if (!*(param->already_assigned_p)) {
-		new_assigned_crtc = (crtc_ll *)
-		    umalloc(sizeof(crtc_ll));
-		new_assigned_crtc->crtc = param->available_crtcs[i];
-		new_assigned_crtc->next = PVAR->assigned_crtc_head;
-		PVAR->assigned_crtc_head = new_assigned_crtc;
-		// printf("New assigned crtc head: %d\n",*(param->assigned_crtc_head_p));
-		*(param->crtc_p) = param->available_crtcs[i];
-	}
-
+	umon_print("ERROR: No available crtc found\n");
+	exit(NO_AVAILABLE_CRTC);
 }
 
 
